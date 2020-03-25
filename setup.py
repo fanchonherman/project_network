09b4be9062139132afac3f0168a16d4228dfb2cd
fanchonherman <60198861+fanchonherman@users.noreply.github.com>
2020-03-25 15:55:16 +0400

    widget of transport

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='networkmtp',
+    version='0.0.1',
+    author='Fanchon Herman , Ryma , Abdesstar',
+    description='Visualisation of a road in Montpellier ',
+    url='https://github.com/fanchonherman/project_network.git',
+    license = 'MIT',
+    packages='networkmtp',
+
+
+)
