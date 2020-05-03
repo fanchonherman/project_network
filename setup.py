c76c3b97e681e192e52689724f82e638546787b5
fanchonherman <60198861+fanchonherman@users.noreply.github.com>
2020-05-03 16:05:52 +0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='montpellier_network',
     version='0.0.1',
-    packages=find_packages(exclude=['tests*']),
+    packages=["network_montpellier", "network_montpellier.make_map"],
     license='MIT',
     author='Fanchon Herman, Sahbane Abdesstar, Ryma Lakehal',
     description='Visualisation of a road in Montpellier ',
