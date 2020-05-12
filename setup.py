2cb47ec79f45d45c0376f0ac37a5067c29d9a166
Abdesstar Sahbane <sahbane.abdesstar@gmail.com>
2020-05-12 11:05:03 +0200

    packing up the package

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    INSTALL_REQUIRES = [l.strip() for l in f.readlines()]
 
 setup(
-    name='networkmtp',
-    version='0.0.1',
-    author='Fanchon Herman , Ryma , Abdesstar',
-    description='Visualisation of a road in Montpellier ',
+    name='Montpellier_Network',
+    version='0.1',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='Visualisation of a road in Montpellier',
+    long_description=open('README.md').read(),
+    install_requires=INSTALL_REQUIRES,
     url='https://github.com/fanchonherman/project_network.git',
-    license = 'MIT',
-    packages='networkmtp',
-
-
+    author='Fanchon Herman, Sahbane Abdesstar, Ryma Lakehal',
+    author_email='sahbane.abdesstar@gmail.com'
 )
