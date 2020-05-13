e8449a2c9c1d4f408fdcc48f46354a27746fda92
fanchonherman <60198861+fanchonherman@users.noreply.github.com>
2020-05-13 15:30:14 +0400

    do the function test

--- network_montpellier/tests/test_network_func.py
+++ network_montpellier/tests/test_network_func.py
@@ -2,20 +2,32 @@
 
 import os.path
 import sys
+#import matplotlib.pyplot as plt
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + (os.path.sep + '..')*2)
 
-import pytest
-import matplotlib.pyplot as plt
+import network_montpellier as net
 
-import network_func
-
-@pytest.mark.mpl_image_compare
-def test_type_transport('walk'):
-    fig = plt.figure()
-    ax = fig.add_subplot(1,1,1)
-    ax.plot([1,2,3])
-    return fig
+#@pytest.mark.mpl_image_compare
+#def test_type_transport('walk'):
+#    fig = plt.figure()
+#    ax = fig.add_subplot(1,1,1)
+#    ax.plot([1,2,3])
+#    return fig
     
 
+def test_distance():
+    assert net.distance_type_transport('bike') == 4339.373999999999
+
+
+def test_animation():
+    assert np.isclose(net.animation_type_transport, 1.97163)
+
+
+def test_times():
+    assert net.times('bike', net.type_transport) == 72.55561256408691
+
+
+def test_type_transport():
+    assert np.isclose(net.type_transport('drive'),1.59513)
 
