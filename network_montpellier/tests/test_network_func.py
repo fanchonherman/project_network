e1182c28f9ac6dd21850ea9bdf9bc59afba0280e
rymlak <49974544+rymlak@users.noreply.github.com>
2020-05-17 01:48:34 +0200

    Add files via upload

--- network_montpellier/tests/test_network_func.py
+++ network_montpellier/tests/test_network_func.py
@@ -2,22 +2,36 @@
 
 import os.path
 import sys
+import pytest
 import numpy as np
-#import matplotlib.pyplot as plt
+import osmnx as ox
+import matplotlib.pyplot as plt
+from matplotlib.testing.decorators import check_figures_equal
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + (os.path.sep + '..')*2)
 import network_montpellier as net
 
-#@pytest.mark.mpl_image_compare
-#def test_type_transport('walk'):
-#    fig = plt.figure()
-#    ax = fig.add_subplot(1,1,1)
-#    ax.plot([1,2,3])
-#    return fig
+
+
 
 def test_distance():
     np.isclose(net.distance_type_transport('bike'), 4339.374)
 
     
+
+@check_figures_equal()
+def test_graph(fig_test, fig_ref):
+     fig_test.net.create_graph('Montpellier, France', 'walk', loc_type="address")
+     fig_ref.ox.graph_from_address('Montpellier, France', network_type='walk') 
+    
+
+
+
+def test_short_path_length():
+    G = ox.graph_from_place('Montpellier, France', network_type='drive')
+    origin = ox.geo_utils.geocode('Maison du Lez, Montpellier, France')
+    destination = ox.geo_utils.geocode('Place Eugène Bataillon, Montpellier, France')
+    assert net.short_path(G, origin, destination)[1] == 55
+
 
 
