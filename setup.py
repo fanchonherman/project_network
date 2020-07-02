021a96cfa331c0f2dea788b4c424c4a4d422dcac
rymlak <49974544+rymlak@users.noreply.github.com>
2020-07-02 10:03:42 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,17 +1,15 @@
-from setuptools import setup, find_packages
-
-with open('requirements.txt') as f:
-    INSTALL_REQUIRES = [l.strip() for l in f.readlines()]
+from setuptools import setup
+from Montpellier_Network import __version__ as current_version
 
 setup(
     name='Montpellier_Network',
-    version='0.0.1',
+    version=current_version,
     packages=["network_montpellier", "network_montpellier.make_map"],
     license='MIT',   
     author='Fanchon Herman, Sahbane Abdesstar, Ryma Lakehal',
     description='Visualisation of a road in Montpellier',
     long_description=open('README.md').read(),
-    install_requires=INSTALL_REQUIRES,
     url='https://github.com/fanchonherman/project_network.git',
     author_email='fanchon.herman974@gmail.com, sahbane.abdesstar@gmail.com '
+    zip_safe=False
 )
